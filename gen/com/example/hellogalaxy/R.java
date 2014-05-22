697a88575fb2839d079938a8f920cec95dae6085
aso1201731 <1201731@st.asojuku.ac.jp>
2014-05-22 14:21:04 +0900

    2014年5月22日 HelloGalaxy 更新 緒方優弥

--- gen/com/example/hellogalaxy/R.java
+++ gen/com/example/hellogalaxy/R.java
@@ -1090,7 +1090,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050044;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1104,6 +1104,7 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int editText1=0x7f05003e;
         public static final int editText2=0x7f050041;
+        public static final int editText3=0x7f050044;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
