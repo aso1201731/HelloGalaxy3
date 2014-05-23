e8a6342b05f6c110fb3001464e75d73b31b2cf6a
aso1201731 <1201731@st.asojuku.ac.jp>
2014-05-23 10:49:19 +0900

    HelloGalaxy 2014年5月23日 更新

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -23,7 +26,27 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()) {
+		case R.id.btnOK:
+			EditText etv1 = (EditText)findViewById(R.id.editFirst);
+				String inputMsg1 = etv1.getText().toString();
+			EditText etv2 = (EditText)findViewById(R.id.editFamiry);
+				String inputMsg2 = etv2.getText().toString();
+
+			TextView tv = (TextView)findViewById(R.id.txvMsg);
+			tv.setText("あなたでしたか、" + inputMsg1 + inputMsg2 + "さん。");
+		}
 
 	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		btnOK.setOnClickListener(this);
+		super.onResume();
+	}
+
+
 
 }
